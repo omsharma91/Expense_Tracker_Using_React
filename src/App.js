2dc2c9ed2d44ae3531f0e3b1904130365446e06d
om sharma <oms570480@gmail.com>
2025-04-01 00:49:35 +0530

    make some change in ui

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import ExpenseTrack from "./expenseTable";
 import ExpenseContainer from "./expenseInput";
 import Navbar from "./navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useReducer, useState } from "react";
-import ProtectedRoute from "./Protected_Route";
-import Profile from "./Profile";
-import Login from "./Login";
+import {useState } from "react";
 import { createContext } from "react";
 export const ExpenseContext = createContext();
 
 function App() {
   const [expenses,setExpenses] = useState([]);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <ExpenseContext.Provider value={{ expenses,setExpenses}}>
       <Router>
@@ -21,15 +17,6 @@ function App() {
           <Route path="/" element={<ExpenseContainer/>} />
           <Route path="/expense_track" element={<ExpenseTrack />} />
           <Route path="/expense_filter" element={<ExpenseFilter />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute isAuth={isAuthenticated}>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
         </Routes>
       </Router>
     </ExpenseContext.Provider>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ExpenseContainer from "./expenseInput";
-import Navbar from "./navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
